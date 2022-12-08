d0cabe6ab759868b9c4bdb16ec7b653d2644c347
ayofalola240 <ayofalola240@gmail.com>
2022-12-08 13:26:21 +0100

    new update

--- src/app.service.ts
+++ src/app.service.ts
@@ -20,93 +20,100 @@ export class AppService {
   }
   async postWebHooks(body: any): Promise<any> {
     // Handle the message, depending on its type
-    if (body.object === 'page') {
-      body.entry.forEach((entry: any) => {
-        const webhook_event = entry.messaging[0];
+    try {
+      if (body.object === 'page') {
+        body.entry.forEach((entry: any) => {
+          const webhook_event = entry.messaging[0];
 
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
+          // Get the sender PSID
+          let sender_psid = webhook_event.sender.id;
 
-        // Handle the event based on its type
-        if (webhook_event.message) {
-          this.handleMessage(sender_psid, webhook_event.message);
-        } else if (webhook_event.postback) {
-          this.handlePostback(sender_psid, webhook_event.postback);
-        }
-      });
-      return {
-        status: 'SUCCESS',
-        data: 'EVENT_RECEIVED',
-      };
-    } else {
-      throw new NotFoundException();
+          // Handle the event based on its type
+          if (webhook_event.message) {
+            this.handleMessage(sender_psid, webhook_event.message);
+          } else if (webhook_event.postback) {
+            this.handlePostback(sender_psid, webhook_event.postback);
+          }
+        });
+        return {
+          status: 'SUCCESS',
+          data: 'EVENT_RECEIVED',
+        };
+      } else {
+        throw new NotFoundException();
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
   async getWebhook(query: any): Promise<any> {
-    let mode = query['hub.mode'];
-    let token = query['hub.verify_token'];
-    let challenge = query['hub.challenge'];
+    try {
+      let mode = query['hub.mode'];
+      let token = query['hub.verify_token'];
+      let challenge = query['hub.challenge'];
 
-    // Check if a token and mode is in the query string of the request
-    if (mode && token) {
-      // Check the mode and token sent is correct
-      if (mode === 'subscribe' && token === this.verify_token) {
-        // Respond with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        return challenge;
+      // Check if a token and mode is in the query string of the request
+      if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === 'subscribe' && token === this.verify_token) {
+          // Respond with the challenge token from the request
+          console.log('WEBHOOK_VERIFIED');
+          return challenge;
+        }
+      } else {
+        throw new NotFoundException();
       }
-    } else {
-      throw new NotFoundException();
+    } catch (error) {
+      console.log(error);
     }
   }
   // Handles messages events
   async handleMessage(sender_psid: any, received_message: any): Promise<any> {
-    let response: any;
-
-    // Check if the message contains text
-    if (received_message.text) {
-      // Create the payload for a basic text message
-      response = {
-        text: `You sent the message: "${received_message.text}". Now send me an image!`,
-      };
-    } else if (received_message.attachments) {
-      // Gets the URL of the message attachment
-      let attachment_url = received_message.attachments[0].payload.url;
-      response = {
-        attachment: {
-          type: 'template',
-          payload: {
-            template_type: 'generic',
-            elements: [
-              {
-                title: 'Is this the right picture?',
-                subtitle: 'Tap a button to answer.',
-                image_url: attachment_url,
-                buttons: [
-                  {
-                    type: 'postback',
-                    title: 'Yes!',
-                    payload: 'yes',
-                  },
-                  {
-                    type: 'postback',
-                    title: 'No!',
-                    payload: 'no',
-                  },
-                ],
-              },
-            ],
-          },
-        },
-      };
-    }
-
-    // Sends the response message
     try {
+      let response: any;
+
+      // Check if the message contains text
+      if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+          text: `You sent the message: "${received_message.text}". Now send me an image!`,
+        };
+      } else if (received_message.attachments) {
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+          attachment: {
+            type: 'template',
+            payload: {
+              template_type: 'generic',
+              elements: [
+                {
+                  title: 'Is this the right picture?',
+                  subtitle: 'Tap a button to answer.',
+                  image_url: attachment_url,
+                  buttons: [
+                    {
+                      type: 'postback',
+                      title: 'Yes!',
+                      payload: 'yes',
+                    },
+                    {
+                      type: 'postback',
+                      title: 'No!',
+                      payload: 'no',
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+        };
+      }
+      // Sends the response message
       return this.callSendAPI(sender_psid, response);
     } catch (error) {
-      console.log(`An error occur ${JSON.stringify(error.data)}`);
+      console.error(error);
     }
   }
 
@@ -118,16 +125,16 @@ export class AppService {
 
   // Sends response messages via the Send API
   async callSendAPI(sender_psid: any, response: any): Promise<any> {
-    // Construct the message body
-    let request_body = {
-      recipient: {
-        id: sender_psid,
-      },
-      message: response,
-    };
-    // Send the HTTP request to the Messenger Platform
     try {
-      const response = await axios({
+      // Construct the message body
+      let request_body = {
+        recipient: {
+          id: sender_psid,
+        },
+        message: response,
+      };
+      // Send the HTTP request to the Messenger Platform
+      const res = await axios({
         method: 'POST',
         url: 'https://graph.facebook.com/v2.6/me/messages',
         headers: {
@@ -135,9 +142,9 @@ export class AppService {
         },
         data: request_body,
       });
-      return response;
+      return res;
     } catch (error) {
-      throw error;
+      console.log(`An error occur ${JSON.stringify(error.data)}`);
     }
   }
 }
