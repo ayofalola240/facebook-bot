278e21c52d02ccc199f922ff5e3472850d1631cf
ayofalola240 <ayofalola240@gmail.com>
2022-12-15 15:27:45 +0100

    new update

--- src/app.service.ts
+++ src/app.service.ts
@@ -116,10 +116,10 @@ export class AppService {
   async getUserName(sender_psid: any): Promise<string> {
     console.log('Gettings profile with sender: ' + sender_psid);
     try {
-      const data: any = await axios.get(
+      const res: any = await axios.get(
         `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${this.page_access_token}`,
       );
-      console.log(data);
+      const { data } = res;
       const userName = `${data.last_name} | ${data.first_name}`;
       return userName;
     } catch (error) {
