b98d9951b6c1877e7640e4614b44c6a68dcf4092
ayofalola240 <ayofalola240@gmail.com>
2022-12-18 17:37:29 +0100

    new update

--- src/app.service.ts
+++ src/app.service.ts
@@ -215,10 +215,7 @@ export class AppService {
       };
       const res = await axios({
         method: 'POST',
-        url: `https://graph.facebook.com/v2.6/${this.pageID}/messages`,
-        headers: {
-          authorization: `Bearer ${this.page_access_token}`,
-        },
+        url: `https://graph.facebook.com/v2.6/${this.pageID}/messenger_profile?access_token=${this.page_access_token}`,
         data: request_body,
       });
 
@@ -239,10 +236,7 @@ export class AppService {
 
       const res = await axios({
         method: 'POST',
-        url: `https://graph.facebook.com/v2.6/${this.pageID}/messages`,
-        headers: {
-          authorization: `Bearer ${this.page_access_token}`,
-        },
+        url: `https://graph.facebook.com/v2.6/${this.pageID}/messenger_profile?access_token=${this.page_access_token}`,
         data: request_body,
       });
       console.log(res);
